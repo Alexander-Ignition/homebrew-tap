546f9affb17aa4c3c7038da1a87701de6d72ff6e
Alexander Ignition <izh.sever@gmail.com>
2020-07-10 17:46:24 +0300

    Create Casks/air-push.rb

---
+++ Casks/air-push.rb
@@ -0,0 +1,10 @@
+cask "air-push" do
+  version "0.0.1"
+  sha256 "28fee48ed8beb5f322ef8a9957f1e4ecef3297df0b0ea0e82fd2b088a0097533"
+
+  url "https://github.com/Alexander-Ignition/AirPush/releases/download/#{version}/air-push"
+  name "AirPush"
+  homepage "https://github.com/Alexander-Ignition/AirPush"
+
+  app "air-push"
+end
